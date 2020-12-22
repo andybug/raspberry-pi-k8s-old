89d8198723d1e379c8d3454d36c347930dd4bae9
Andrew Fields <andy@andybug.com>
2020-12-22 08:50:43 -0600

    change cert signing to 30 days

--- scripts/pf-vault.zsh
+++ scripts/pf-vault.zsh
@@ -3,5 +3,6 @@
 SERVICE_PORT=8200
 LOCAL_PORT=8200
 
-/bin/zsh -c "sleep 2; xdg-open https://127.0.0.1:$LOCAL_PORT" &
-exec kubectl -n vault port-forward svc/vault-ui $LOCAL_PORT:$SERVICE_PORT
+echo export VAULT_ADDR='https://127.0.0.1:8200'
+echo export VAULT_CACERT=/etc/ssl/certs/weirwd_net_k8s0.pem
+exec kubectl -n vault port-forward svc/vault $LOCAL_PORT:$SERVICE_PORT
